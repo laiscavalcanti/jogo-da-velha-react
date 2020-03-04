5fdd72efed37ef78fa2346418f3fe9c8bc482f5f
Laís Cavalcanti <cavalcantislais@gmail.com>
2020-03-04 00:32:16 -0300

    playerO adicionado

--- game-react/src/objects/Player/index.js
+++ game-react/src/objects/Player/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './style.css';
 import playerX from '../../images/X.png';
+import playerO from '../../images/Circle.png';
 
 const Player = () => (
     <button className="player">
-        <img src={playerX} />
+        <img src={playerX} alt="jogador X" />
     </button>
     
 )
